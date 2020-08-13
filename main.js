a8290865315ef665bd7b77a9836a42f91ae6e15c
ericjt505 <weirdspacetobe@gmail.com>
2020-08-13 17:48:35 -0400

    updated 8-13-20 @ 5:48pm

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// COMPLETE VARIABLE AND FUNCTION DEFINITIONS
+
 var customName = document.getElementById('customname');
 var randomize = document.querySelector('.randomize');
 var story = document.querySelector('.story');
@@ -6,10 +8,14 @@ function randomValueFromArray(array){
   return array[Math.floor(Math.random()*array.length)]
 }
 
+// RAW TEXT STRINGS
+
 let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 let insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
 let insertY = ['the soup kitchen','Disneyland','the White House'];
 let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
+
+// EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
 
 randomize.addEventListener('click', result);
 
